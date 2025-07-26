cd7d0102bece6c47fbb72a2aad78d4d35be0f618
Andrew Wittrock <andy@wittrock.us>
2025-07-26 12:54:57 -0500

    Move subscriptions to database

--- components/SignUpForm/SignUpForm.container.ts
+++ components/SignUpForm/SignUpForm.container.ts
@@ -1,17 +1,24 @@
-import { createInjector, inject, mergeProps } from "unstateless";
-import {SignUpFormComponent} from "./SignUpForm.component";
-import {ISignUpFormInputProps, SignUpFormProps, ISignUpFormProps} from "./SignUpForm.d";
-import { useState } from "react";
-import { useLoggedInUser } from "@uac/lib/login/services";
 import { services } from "@core/lib/api";
-import { all } from "ts-functional";
 import { flash } from "@core/lib/flash";
-import { localConfig } from "../../../config.local";
+import { useLoaderAsync } from "@core/lib/useLoader";
+import { ISubscriptionPlan } from "@subscription-shared/types";
+import { useLoggedInUser } from "@uac/lib/login/services";
+import { useEffect, useState } from "react";
+import { all } from "ts-functional";
+import { createInjector, inject, mergeProps } from "unstateless";
+import { SignUpFormComponent } from "./SignUpForm.component";
+import { ISignUpFormInputProps, ISignUpFormProps, SignUpFormProps } from "./SignUpForm.d";
 
 const injectSignUpFormProps = createInjector(({}:ISignUpFormInputProps):ISignUpFormProps => {
     const [selectedOption, setSelectedOption] = useState<number>(0);
     const [user, _setUser, refresh] = useLoggedInUser();
+    const [plans, setPlans] = useState<ISubscriptionPlan[]>([]);
+    const loader = useLoaderAsync();
     
+    useEffect(() => {
+        loader(() => services().subscription.plans.search().then(setPlans))
+    }, []);
+
     const createSubscription = (option:any) => (data:any, actions:any) => {
         console.log("Create subscription", data, actions);
         return actions.subscription.create({
@@ -24,8 +31,6 @@ const injectSignUpFormProps = createInjector(({}:ISignUpFormInputProps):ISignUpF
         return services().user.subscribe(user.user.id, data.subscriptionID)
             .then(all(flash.success("Subscription created"), refresh));
     }
-
-    const plans = localConfig.paypal.plans;
 
     return {selectedOption, setSelectedOption, userId: user.user.id, createSubscription, onApprove, plans};
 });
