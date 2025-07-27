6c833d3b9d77526dc8a1fa476d6745a70fe65f3f
Andrew Wittrock <andy@wittrock.us>
2025-07-26 20:56:43 -0500

    Add subscription settings

--- index.tsx
+++ index.tsx
@@ -2,6 +2,7 @@ import { IModule } from "@core/lib/module";
 import { faDollarSign } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { PlanManager } from "./components/PlanManager";
+import { subscriptionSettings } from "./lib/settings";
 
 export const module:IModule = {
     name: "subscription",
@@ -16,5 +17,6 @@ export const module:IModule = {
         admin: [
             {path: "/subscription-plans", component: PlanManager},
         ]
-    }
+    },
+    settings: subscriptionSettings,
 };
---
+++ lib/settings.ts
@@ -0,0 +1,20 @@
+import { ISettingContainer } from "@common/lib/setting/types";
+
+export const subscriptionSettings:ISettingContainer = {
+    "Subscription": {
+        "General": {
+            subscriptionName: {
+                displayName: "Subscription Name",
+                type: "string",
+                defaultValue: "My Subscription",
+                description: "The name of the subscription as it appears in emails and other communications.",
+            },
+            subscripttionInfoUrl: {
+                displayName: "Subscription Info URL",
+                type: "string",
+                defaultValue: "",
+                description: "The URL for the subscription info page.",
+            }
+        },
+    },
+};
